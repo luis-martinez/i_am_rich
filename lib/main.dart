86759aa04710930e38efeeab153f6999e34e655b
Luis Martinez <luis-martinez@users.noreply.github.com>
2020-07-12 07:40:19 -0700

    Added Network Image.

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World!'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flokeshdhakar.com%2Fprojects%2Flightbox2%2Fimages%2Fimage-3.jpg&f=1&nofb=1'),
+          ),
+        ),
       ),
     ),
   );
